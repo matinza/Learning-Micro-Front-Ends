d699e2d98e732937ce71d9825298581a903529f6
matan avital <matinza.avital@gmail.com>
2023-11-01 18:54:25 +0200

    learned phases 2 & 3

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "./index.scss";
+import './index.scss';
+
+import Footer from './Footer';
+import Header from './Header';
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: home</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div>Home Page Content</div>
+    <Footer />
   </div>
 );
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,14 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import "./index.scss";
+import Footer from 'home/Footer';
+import Header from 'home/Header';
+import './index.scss';
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: pdp</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div>Home Page Content</div>
+    <Footer />
   </div>
 );
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
